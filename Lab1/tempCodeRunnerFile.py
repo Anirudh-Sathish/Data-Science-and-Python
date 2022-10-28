70c68cac8278480c65acc1d5d06bc673e99071a3
Anirudh Sathish <81130487+Anirudh-Sathish@users.noreply.github.com>
2022-10-29 01:02:36 +0530

    Delete tempCodeRunnerFile.py

--- Lab1/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-    arr.append(i)
-
-#Appling the sort function on the array 
-insertionSort(arr)
-
-#Asking the user to input a search parameter
-search_parameter = int(input("What is the number you are searching for "))
